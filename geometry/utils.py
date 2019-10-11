70ea70635a95b57b62ec1d307447d623cffa4fdb
Alexandra Diem <alexandra.diem@gmail.com>
2019-10-11 08:27:57 +0200

    added load_geometry_from_h5

--- geometry/utils.py
+++ geometry/utils.py
@@ -48,3 +48,88 @@ def make_logger(name, level=df.get_log_level()):
 
 def set_namedtuple_default(NamedTuple, default=None):
     NamedTuple.__new__.__defaults__ = (default,) * len(NamedTuple._fields)
+
+
+def load_geometry_from_h5(h5name, h5group="", fendo=None, fepi=None,
+                            include_sheets=True, comm=df.MPI.comm_world,
+):
+    """Load geometry and other mesh data from
+    a h5file to an object.
+    If the file contains muliple fiber fields
+    you can spefify the angles, and if the file
+    contais sheets and cross-sheets this can also
+    be included
+
+    :param str h5name: Name of the h5file
+    :param str h5group: The group within the file
+    :param int fendo: Helix fiber angle (endocardium) (if available)
+    :param int fepi: Helix fiber angle (epicardium) (if available)
+    :param bool include_sheets: Include sheets and cross-sheets
+    :returns: An object with geometry data
+    :rtype: object
+
+    """
+
+    logger.info("\nLoad mesh from h5")
+    # Set default groups
+    ggroup = "{}/geometry".format(h5group)
+    mgroup = "{}/mesh".format(ggroup)
+    lgroup = "{}/local basis functions".format(h5group)
+    fgroup = "{}/microstructure/".format(h5group)
+
+    if not os.path.isfile(h5name):
+        raise IOError("File {} does not exist".format(h5name))
+
+    # Check that the given file contains
+    # the geometry in the given h5group
+    if not io_utils.check_h5group(h5name, mgroup, delete=False, comm=comm):
+        msg = ("Warning!\nGroup: '{}' does not exist in file:" "\n{}").format(
+            mgroup, h5name
+        )
+
+        with h5py.File(h5name) as h:
+            keys = h.keys()
+        msg += "\nPossible values for the h5group are {}".format(keys)
+        raise IOError(msg)
+
+    # Create a dummy object for easy parsing
+    class Geometry(object):
+        pass
+
+    geo = Geometry()
+
+    with dolfin.HDF5File(comm, h5name, "r") as h5file:
+
+        # Load mesh
+        mesh = dolfin.Mesh(comm)
+        io_utils.read_h5file(h5file, mesh, mgroup, True)
+        geo.mesh = mesh
+
+        # Get mesh functions
+        for dim, attr in zip(range(4), ["vfun", "efun", "ffun", "cfun"]):
+
+            dgroup = "{}/mesh/meshfunction_{}".format(ggroup, dim)
+            mf = dolfin.MeshFunction("size_t", mesh, dim, mesh.domains())
+
+            if h5file.has_dataset(dgroup):
+                io_utils.read_h5file(h5file, mf, dgroup)
+            setattr(geo, attr, mf)
+
+        load_local_basis(h5file, lgroup, mesh, geo)
+        load_microstructure(h5file, fgroup, mesh, geo, include_sheets)
+
+        # Load the boundary markers
+        markers = load_markers(h5file, mesh, ggroup, dgroup)
+        geo.markers = markers
+
+        origmeshgroup = "{}/original_geometry".format(h5group)
+        if h5file.has_dataset(origmeshgroup):
+            original_mesh = dolfin.Mesh(comm)
+            io_utils.read_h5file(h5file, original_mesh, origmeshgroup, True)
+            setattr(geo, "original_geometry", original_mesh)
+
+    for attr in ["f0", "s0", "n0", "r0", "c0", "l0", "cfun", "vfun", "efun", "ffun"]:
+        if not hasattr(geo, attr):
+            setattr(geo, attr, None)
+
+    return geo
