9599cd7fc2beff8644bdd189c9a35a1ab1efeaca
Alexandra Diem <alexandra.diem@gmail.com>
2019-10-11 07:55:07 +0200

    added Geometry2D and HearGeometry skeletons

--- geometry/geometry.py
+++ geometry/geometry.py
@@ -6,30 +6,93 @@ from collections import namedtuple
 
 from dolfin import (MeshFunction)
 
-from .utils import set_namedtuple_default
+from .utils import (make_logger, set_namedtuple_default,
+                    load_geometry_from_h5)
+
+logger = make_logger(__name__, parameters["log_level"])
 
 
-Markers = namedtuple('Marker', ['label', 'value', 'dim'])
+Markers = namedtuple('Markers', ['label', 'value', 'dim'])
 set_namedtuple_default(Markers)
+
+MarkerFunctions = namedtuple("MarkerFunctions", ["vfun", "efun", "ffun", "cfun"])
+set_namedtuple_default(MarkerFunctions)
+
+Microstructure = namedtuple("Microstructure", ["f0", "s0", "n0"])
+set_default_none(Microstructure)
+
+
+def get_attribute(obj, key1, key2, default=None):
+    f = getattr(obj, key1, None)
+    if f is None:
+        f = getattr(obj, key2, default)
+    return f
 
 
 class Geometry(object):
 
-    def __init__(self, mesh=None, markers=None):
+    def __init__(self, mesh=None, markers=None, markerfunctions=None,
+                    microstructure=None):
         self.mesh = mesh or {}
         self.markers = markers or {}
+        self.markerfunctions = markerfunctions or {}
+        self.microstructure = microstructure or {}
 
 
-    def add_mesh(self, physics, mesh, markers=None):
+    def add_mesh(self, label, mesh, markers=None):
         try:
-            self.mesh[physics] = mesh
-            self.markers[physics] = markers
+            self.mesh[label] = mesh
+            self.markers[label] = markers or Markers()
+            self.markerfunctions[label] = markerfunctions or MarkerFunctions()
+            self.microstructure[label] = microstructure or None
         except TypeError:
             msg = "Can only set up multi-mesh geometry if Geometry() has been instantiated empty."
             raise TypeError(msg)
+
+
+    @classmethod
+    def from_file(cls, h5name, h5group="", comm=None):
+        comm = comm if comm is not None else mpi_comm_world()
+        return cls(**cls.load_from_file(h5name, h5group, comm))
 
 
 class Geometry2D(Geometry):
 
     def __init__(self, *args, **kwargs):
         super(Geometry2D, self).__init__(*args, **kwargs)
+
+
+class HeartGeometry(Geometry):
+
+    def __init__(self, *args, **kwargs):
+        super(HeartGeometry, self).__init__(*args, **kwargs)
+
+
+    @staticmethod
+    def load_from_file(h5name, h5group, comm):
+
+        logger.debug("Load geometry from file {}".format(h5name))
+
+        geo = load_geometry_from_h5(h5name, h5group, include_sheets=True, comm=comm)
+
+        f0 = get_attribute(geo, "f0", "fiber", None)
+        s0 = get_attribute(geo, "s0", "sheet", None)
+        n0 = get_attribute(geo, "n0", "sheet_normal", None)
+
+        c0 = get_attribute(geo, "c0", "circumferential", None)
+        r0 = get_attribute(geo, "r0", "radial", None)
+        l0 = get_attribute(geo, "l0", "longitudinal", None)
+
+        vfun = get_attribute(geo, "vfun", None)
+        ffun = get_attribute(geo, "ffun", None)
+        cfun = get_attribute(geo, "cfun", "sfun", None)
+
+        kwargs = {
+            "mesh": geo.mesh,
+            "markers": geo.markers,
+            "markerfunctions": MarkerFunctions(vfun=vfun, ffun=ffun, cfun=cfun),
+            "microstructure": Microstructure(f0=f0, s0=s0, n0=n0),
+            "crl_basis": CRLBasis(c0=c0, r0=r0, l0=l0),
+        }
+
+        return kwargs
--- geometry/utils.py
+++ geometry/utils.py
@@ -9,5 +9,41 @@ def dict_to_namedtuple(d, NamedTuple):
     pass
 
 
+def make_logger(name, level=parameters["log_level"]):
+    def log_if_process0(record):
+        if dolfin.MPI.rank(mpi_comm_world()) == 0:
+            return 1
+        else:
+            return 0
+
+    mpi_filt = Object()
+    mpi_filt.filter = log_if_process0
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(0)
+    # formatter = logging.Formatter('%(message)s')
+    formatter = logging.Formatter(
+        ("%(asctime)s - " "%(name)s - " "%(levelname)s - " "%(message)s")
+    )
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.addFilter(mpi_filt)
+
+    dolfin.set_log_level(logging.WARNING)
+
+    ffc_logger = logging.getLogger("FFC")
+    ffc_logger.setLevel(logging.WARNING)
+    ffc_logger.addFilter(mpi_filt)
+
+    ufl_logger = logging.getLogger("UFL")
+    ufl_logger.setLevel(logging.WARNING)
+    ufl_logger.addFilter(mpi_filt)
+
+    return logger
+
+
 def set_namedtuple_default(NamedTuple, default=None):
     NamedTuple.__new__.__defaults__ = (default,) * len(NamedTuple._fields)
