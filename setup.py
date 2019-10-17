38b2e69181a77e0a053406aa54b26c4cce59c70a
Alexandra Diem <alexandra.diem@gmail.com>
2019-10-17 11:10:08 +0200

    removed version from install_requires

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='fenics-geometry',
       version='2019.1.0',
       description='A library handling geometries for Fenics-based problems. Based on pulse.geometry by Henrik Finsberg',
-      url='https://github.com/KVSLab/fenics-geometry',
+      url='https://github.com/ComputationalPhysiology/fenics-geometry',
       author='Alexandra K. Diem',
       author_email='alexandra@simula.no',
       license='LGPL3',
       packages=['geometry'],
-      install_requires=['pytest', 'fenics=2019.1.0=py37_5'],
+      install_requires=['pytest', 'fenics'],
       zip_safe=False)
